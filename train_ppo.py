044550fa455ec783d4a6a35f906a9ac056543fd7
Nathaniel <41310074+Natchan03@users.noreply.github.com>
2021-04-23 01:16:44 -0400

    Modified train_ppo.py for easier read/write

--- train_ppo.py
+++ train_ppo.py
@@ -16,6 +16,15 @@ import copy
 
 import tensorflow as tf
 
+# loads a model from checkpoint
+def getLastCheckpoint(load_dir):
+    with open(os.path.join(load_dir, 'checkpoint'), 'r') as f:
+        line = f.readline()
+        start = line.find('\"') + 1
+        end = line.rfind('\"')
+        path = line[start:end]
+        return path
+
 
 def run(env, algorithm, args, params=None, load=False, loadpath=None, loaditer=None, save_obs=False):
 
@@ -28,10 +37,14 @@ def run(env, algorithm, args, params=None, load=False, loadpath=None, loaditer=N
     ppo.initialize()
 
     # load trained policy
-    if load and loadpath is not None and loaditer is not None:
+    if load and loadpath is not None:
         pi = init_policy
-        pi.load_model(loadpath, iteration=loaditer)
-        pi.save_model(args['MODEL_DIR'], iteration=0)
+        # pi.load_model(loadpath, iteration=loaditer)
+        pi.load_model_from_path(getLastCheckpoint(loadpath))
+        if loaditer is None:
+            loaditer = 0
+        pi.save_model(args['MODEL_DIR'], iteration=loaditer)
+            
     else:
         # init policy
         pi = init_policy
@@ -87,7 +100,7 @@ if __name__ == "__main__":
         # --- logger initialize and configuration ---
         RUN_DIR = MODEL_DIR = FIGURE_DIR = RESULT_DIR = None
         if args['algo'] == "ppo":
-            RUN_DIR = os.path.join(os.getcwd(), 'runs_log_tests', strftime('%d-%b-%Y_%H-%M-%S') + args['gym_env'] + '_' + args['algo'])
+            RUN_DIR = os.path.join(os.getcwd(), 'runs', 'ppo_ttr')
             MODEL_DIR = os.path.join(RUN_DIR, 'model')
             FIGURE_DIR = os.path.join(RUN_DIR, 'figure')
             RESULT_DIR = os.path.join(RUN_DIR, 'result')
@@ -110,7 +123,7 @@ if __name__ == "__main__":
             maybe_mkdir(args['MODEL_DIR'])
             maybe_mkdir(args['FIGURE_DIR'])
             maybe_mkdir(args['RESULT_DIR'])
-            ppo_params_json = './ppo1/ppo_params.json'
+            ppo_params_json = 'params.json'
 
             # --- Start to train the policy from scratch ---
             # env = gym.make(args['gym_env'], rew=args['rew_type'])
@@ -122,11 +135,14 @@ if __name__ == "__main__":
             # LOAD_DIR =  os.environ['PROJ_HOME_3'] + '/runs_log_tests/grad_norm_0.5_kl_0.015_std_0.5_fixed_value_vi/23-Jan-2020_00-13-24DubinsCarEnv-v0_hand_craft_ppo_vf_boltzmann/model'
             # eval_policy = run(env=env, algorithm=ppo, params=ppo_params_json, load=True, loadpath=LOAD_DIR, loaditer=180, args=args)
 
-            # --- Load pre-trained model and continue training ---
+            # String of folder containing this file
+            curFolder = os.path.dirname(os.path.abspath(__file__))
+
+            # --- Load pre-trained model and continue training---
             env = gym.make(args['gym_env'], rew=args['rew_type'])
-            LOAD_DIR = '/local-scratch/xlv/quad_stabilization' + '/runs_log_tests/18-Apr-2020_00-22-43QuadTakeOffHoverEnv-v0_ppo/model'
+            LOAD_DIR = curFolder + '/runs/ppo_ttr/model'
             trained_policy = run(env=env, algorithm=ppo, params=ppo_params_json, load=True, loadpath=LOAD_DIR,
-                              loaditer=420, args=args)
+                              loaditer=None, args=args) # loaditer is None to load trained_model file
             trained_policy.save_model(args['MODEL_DIR'])
         else:
             raise ValueError("arg algorithm is invalid!")
